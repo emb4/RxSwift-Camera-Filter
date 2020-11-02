44e77d981482a782de12f00a50b7cee0031d0063
Eric Bates <37354656+emb4@users.noreply.github.com>
2020-11-02 06:52:06 -0500

    Commit of project

---
+++ Camera Filter/FilterService.swift
@@ -0,0 +1,48 @@
+//
+//  FilterService.swift
+//  Camera Filter
+//
+//  Created by Eric Bates on 11/1/20.
+//
+
+import Foundation
+import UIKit
+import CoreImage
+import RxSwift
+
+class FilterService {
+    private var context: CIContext
+    
+    init() {
+        self.context = CIContext()
+    }
+    
+    func applyFilter(to inputImage: UIImage) -> Observable<UIImage> {
+        
+        return Observable<UIImage>.create { observer in
+            
+            self.applyFilter(to: inputImage) { filteredImage in
+                observer.onNext(filteredImage)
+            }
+            
+            return Disposables.create()
+        }
+    }
+    
+    func applyFilter(to inputImage: UIImage, completion: @escaping ((UIImage) -> ())) {
+        
+        let filter = CIFilter(name: "CICMYKHalftone")!
+        filter.setValue(5.0, forKey: kCIInputWidthKey)
+        
+        if let sourceImage = CIImage(image: inputImage) {
+            
+            filter.setValue(sourceImage, forKey: kCIInputImageKey)
+            
+            if let cgimg = self.context.createCGImage(filter.outputImage!, from: filter.outputImage!.extent) {
+                
+                let processedImage = UIImage(cgImage: cgimg, scale: inputImage.scale, orientation: inputImage.imageOrientation)
+                completion(processedImage)
+            }
+        }
+    }
+}
---
+++ Camera Filter/PhotoCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Camera Filter
+//
+//  Created by Eric Bates on 10/27/20.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+}
